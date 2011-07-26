2f8e5cb96bec45a1df2552c3628c2266c5533e54
QW3 <contato@qw3.com.br>
2011-07-26 13:13:43 -0700

    Inclusão de path /app e transformação em Engine

--- lib/qw3chat.rb
+++ lib/qw3chat.rb
@@ -1,2 +1,10 @@
 require "qw3chat/version"
 require "inflections.rb"
+
+module Qw3chat
+
+  class Qw3chat < Rails::Engine
+    config.autoload_paths << File.expand_path("../app", __FILE__)
+  end
+  
+end 
