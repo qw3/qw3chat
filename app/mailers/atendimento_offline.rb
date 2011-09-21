3d57ecd614b04b298d4977789c024590a1ad831b
Marcelo Theodoro <marcelo@qw3.com.br>
2011-09-21 11:24:56 -0300

    Mailer usava configuração antiga sem Settings

--- app/mailers/atendimento_offline.rb
+++ app/mailers/atendimento_offline.rb
@@ -6,7 +6,7 @@ class AtendimentoOffline < ActionMailer::Base
     @cliente        = cliente
     @mensagem       = mensagem
     @assunto        = '[Atendimento online] - Cliente enviou uma mensagem enquanto atendimento estava offline'
-    email_to = (!departamento.nil?) ? departamento.email : ConfiguracaoAtendimento.email_padrao
+    email_to = (!departamento.nil?) ? departamento.email : Settings["QW3CHAT.email_padrao"]
     
     mail(:to => email_to, :subject => @assunto, :from => 'contato@qw3.com.br') do |format|
       format.html
--- lib/qw3chat/version.rb
+++ lib/qw3chat/version.rb
@@ -1,4 +1,4 @@
 # -*- encoding : utf-8 -*-
 module Qw3chat
-  VERSION = "1.1.2"
+  VERSION = "1.1.3"
 end
