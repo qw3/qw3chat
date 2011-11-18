ad52ea27162fad3801f6ddf50b11786595792885
Marcelo - QW3 <contato@qw3.com.br>
2011-11-17 22:59:11 -0200

    Permissionamento de atendente

--- app/models/chat_atendente.rb
+++ app/models/chat_atendente.rb
@@ -3,4 +3,14 @@ class ChatAtendente < Administrador
   belongs_to :chat_departamento
   attr_accessible :chat_departamento, :chat_departamento_id
   
+  after_create :dar_permissao_de_atendente
+  
+  def dar_permissao_de_atendente
+    # se existe permissionamento de usuário    
+    if self.methods.include? :permissoes
+      Permissao.create(:nome => 'Atendente') if Permissao.find_by_nome('Atendente').nil? # cria a permissão de atendente se não existe
+      self.permissoes.push Permissao.find_by_nome('Atendente')
+    end
+  end
+  
 end
